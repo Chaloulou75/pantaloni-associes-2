4e269a1c4354219f79b8392dc861b1ea18abe629
Charles Jeandey <charlesjeandey@MacBook-Air-de-Charles.local>
2025-01-10 12:45:59 +0100

    photo tribunal

--- components/ExpertiseComponent.vue
+++ components/ExpertiseComponent.vue
@@ -2,24 +2,24 @@
 <template>
   <div class="min-h-full pb-28" id="expertise">
     <h2
-      class="sticky top-0 z-30 py-4 text-xl font-medium tracking-wider text-center uppercase bg-gray-50/50 backdrop-blur-xs"
+      class="sticky top-0 z-30 bg-gray-50/50 py-4 text-center text-xl font-medium uppercase tracking-wider backdrop-blur-xs"
     >
       Expertise
     </h2>
     <div
-      class="relative items-center justify-between h-full my-8 lg:flex lg:flex-row lg:items-start lg:px-6"
+      class="relative my-8 h-full items-center justify-between lg:flex lg:flex-row lg:items-start lg:px-6"
     >
       <div class="w-full px-2 lg:sticky lg:top-20 lg:my-36 lg:w-1/2">
         <NuxtImg
           sizes="sm:95vw md:50vw lg:400px"
-          src="/tribunal.jpg"
+          src="/photo_palais.jpg"
           alt="tribunal"
-          class="w-full mx-auto rounded-md shadow-lg shadow-sky-800/60 md:rounded-md"
+          class="mx-auto w-full rounded-md shadow-lg shadow-sky-800/60 md:rounded-md"
         />
       </div>
       <div class="w-full px-4 py-4 text-black lg:w-1/2 lg:px-8">
-        <div class="w-1/4 mx-auto my-8 border-t border-red-500"></div>
-        <p class="py-2 mx-auto text-xl tracking-wide text-justify indent-8">
+        <div class="mx-auto my-8 w-1/4 border-t border-red-500"></div>
+        <p class="mx-auto py-2 text-justify indent-8 text-xl tracking-wide">
           Depuis sa constitution, le Cabinet
           <span class="font-semibold text-red-500">
             PANTALONI GREINER RACHWAN
@@ -31,7 +31,7 @@
           >
           dans ses domaines d’interventions.
         </p>
-        <p class="py-2 mx-auto text-xl tracking-wide text-justify indent-8">
+        <p class="mx-auto py-2 text-justify indent-8 text-xl tracking-wide">
           Le Cabinet dispose d’une compétence reconnue en
           <span
             class="font-semibold underline decoration-red-500 decoration-2 underline-offset-2"
@@ -51,14 +51,14 @@
             >Droit des assurances</span
           >.
         </p>
-        <div class="py-6 mx-auto">
-          <ul class="space-y-4 text-xl tracking-wide list-disc list-inside">
+        <div class="mx-auto py-6">
+          <ul class="list-inside list-disc space-y-4 text-xl tracking-wide">
             <li
               class="font-semibold underline decoration-red-500 decoration-2 underline-offset-2"
             >
               Droit de la responsabilité civile
             </li>
-            <p class="mx-auto text-xl tracking-wide text-justify indent-8">
+            <p class="mx-auto text-justify indent-8 text-xl tracking-wide">
               Le Cabinet
               <span class="font-semibold text-red-500"
                 >PANTALONI GREINER RACHWAN</span
@@ -70,7 +70,7 @@
               >, des <span class="font-semibold">agents immobiliers</span> et
               des <span class="font-semibold">experts judiciaires</span>.
             </p>
-            <p class="mx-auto text-xl tracking-wide text-justify indent-8">
+            <p class="mx-auto text-justify indent-8 text-xl tracking-wide">
               Par ailleurs, il a développé une activité en matière d’<span
                 class="font-semibold"
                 >accidents de la circulation</span
@@ -85,7 +85,7 @@
             >
               Droit de la construction
             </li>
-            <p class="mx-auto text-xl tracking-wide text-justify indent-8">
+            <p class="mx-auto text-justify indent-8 text-xl tracking-wide">
               Le Cabinet
               <span class="font-semibold text-red-500"
                 >PANTALONI GREINER RACHWAN</span
@@ -96,7 +96,7 @@
               chantier, désordres, malfaçons, non-conformité, litige entreprise,
               sous-traitant).
             </p>
-            <p class="mx-auto text-xl tracking-wide text-justify indent-8">
+            <p class="mx-auto text-justify indent-8 text-xl tracking-wide">
               Il assure dans ce cadre le suivi des expertises judiciaires et du
               contentieux jusqu’au stade de l’exécution.
             </p>
@@ -105,7 +105,7 @@
             >
               Droit pénal et réparation du préjudice corporel
             </li>
-            <p class="mx-auto text-xl tracking-wide text-justify indent-8">
+            <p class="mx-auto text-justify indent-8 text-xl tracking-wide">
               Outre le <span class="font-semibold">droit pénal général</span>,
               le Cabinet
               <span class="font-semibold text-red-500"
@@ -117,13 +117,13 @@
               >, ainsi qu’en
               <span class="font-semibold">droit pénal du travail</span>.
             </p>
-            <p class="mx-auto text-xl tracking-wide text-justify indent-8">
+            <p class="mx-auto text-justify indent-8 text-xl tracking-wide">
               Ce faisant, il assiste tant les entreprises que les particuliers,
               qu’ils soient <span class="font-semibold">prévenus</span> ou
               <span class="font-semibold">parties civiles</span>, à tous les
               stades de la procédure.
             </p>
-            <p class="mx-auto text-xl tracking-wide text-justify indent-8">
+            <p class="mx-auto text-justify indent-8 text-xl tracking-wide">
               Le Cabinet
               <span class="font-semibold text-red-500"
                 >PANTALONI GREINER RACHWAN</span
@@ -137,7 +137,7 @@
             >
               Droit des assurances
             </li>
-            <p class="mx-auto text-xl tracking-wide text-justify indent-8">
+            <p class="mx-auto text-justify indent-8 text-xl tracking-wide">
               Le Cabinet
               <span class="font-semibold text-red-500"
                 >PANTALONI GREINER RACHWAN</span
@@ -146,7 +146,7 @@
               <span class="font-semibold">contrat d’assurance</span> (assurance
               responsabilité et construction).
             </p>
-            <p class="mx-auto text-xl tracking-wide text-justify indent-8">
+            <p class="mx-auto text-justify indent-8 text-xl tracking-wide">
               Il met en jeu les
               <span class="font-semibold">garanties légales</span>: garantie de
               parfait achèvement, garantie décennale, garantie biennale ainsi
@@ -157,7 +157,7 @@
             </p>
           </ul>
         </div>
-        <div class="w-1/4 mx-auto my-8 border-t border-red-500"></div>
+        <div class="mx-auto my-8 w-1/4 border-t border-red-500"></div>
       </div>
     </div>
   </div>
